8ff190e9c11fafa11c5696d8793ccf81893967c4
Joshua Blum <joshblum@mit.edu>
2013-11-17 23:51:40 -0500

    package changes

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 
 distutils.core.setup(
     name="tmdb",
-    packages = ["tmdb"],
-    install_requires = ['requests>=0.11.1'],    
-    )
+    packages=["tmdb"],
+    install_requires=['requests>=0.11.1'],
+)
